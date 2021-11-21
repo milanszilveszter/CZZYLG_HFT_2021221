b968ed63438138d5da8a7dc9840c4a6905c9f17f
Szilveszter Milán <milan.szilveszter@stud.uni-obuda.hu>
2021-11-21 18:20:11 +0100

    Student logic interface created

---
+++ CZZYLG_HFT_2021221.Logic/IStudentLogic.cs
@@ -0,0 +1,19 @@
+﻿using CZZYLG_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZZYLG_HFT_2021221.Logic
+{
+    interface IStudentLogic
+    {
+        void Create(Student course);
+        IQueryable<Student> ReadAll();
+        void Update(Student course);
+        void Delete(int courseId);
+
+        double AverageGrade();
+    }
+}
