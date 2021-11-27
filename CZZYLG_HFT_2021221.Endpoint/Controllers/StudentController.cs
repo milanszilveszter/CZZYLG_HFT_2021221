4ee0e6659de57bdd2e68661975e201a794d3582e
Szilveszter Milán <milan.szilveszter@stud.uni-obuda.hu>
2021-11-27 15:58:08 +0100

    StudentController created.

---
+++ CZZYLG_HFT_2021221.Endpoint/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using CZZYLG_HFT_2021221.Logic;
+using CZZYLG_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CZZYLG_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IStudentLogic logic;
+
+        public StudentController(IStudentLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            return "TEST";
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            return logic.ReadAll();
+        }
+
+        [HttpPost]
+        public void AddOne([FromBody] Student student)
+        {
+            logic.Create(student);
+        }
+
+        [HttpPut]
+        public void EditOne([FromBody] Student student)
+        {
+            logic.Update(student);
+        }
+
+        [HttpDelete("{studentId}")]
+        public void DeleteOne([FromRoute] int studentId)
+        {
+            logic.Delete(studentId);
+        }
+    }
+}
--- CZZYLG_HFT_2021221.Endpoint/Startup.cs
+++ CZZYLG_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using CZZYLG_HFT_2021221.Data;
+using CZZYLG_HFT_2021221.Logic;
+using CZZYLG_HFT_2021221.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,15 @@ namespace CZZYLG_HFT_2021221.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddTransient<IStudentLogic, StudentLogic>();
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<ITeacherLogic, TeacherLogic>();
+            services.AddTransient<ITeacherRepository, TeacherRepository>();
+            services.AddTransient<ICourseLogic, CourseLogic>();
+            services.AddTransient<ICourseRepository, CourseRepository>();
+            services.AddTransient<SchoolContext, SchoolContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +37,16 @@ namespace CZZYLG_HFT_2021221.Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
